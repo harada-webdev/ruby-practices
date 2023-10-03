60199c31bb71d6424dc60b20a0aed2ba3eeca3d9
harada-webdev <hrdk74531@outlook.jp>
2023-10-04 04:16:31 +0900

    Add bowling

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 score = ARGV[0]
 scores = score.split(',')
 shots = []
@@ -17,21 +19,25 @@ end
 
 point = 0
 frames.each_with_index do |frame, index|
-  if frame[0] == 10 # strike
-    if index < 9
-      if frames[index + 1][0] == 10
-        point += 20 + frames[index + 2][0]
-      else
-        point += 10 + frames[index + 1].sum
-      end
-    else
-      point += frame.sum
-    end
-  elsif frame.sum == 10 # spare
-    point += 10 + frames[index + 1][0]
-  else
-    point += frame.sum
-  end
+  point += if frame[0] == 10 # strike
+             if index < 9
+               if frames[index + 1][0] == 10
+                 20 + frames[index + 2][0]
+               else
+                 10 + frames[index + 1].sum
+               end
+             else
+               frame.sum
+             end
+           elsif frame.sum == 10 # spare
+             if index < 9
+               10 + frames[index + 1][0]
+             else
+               frame.sum
+             end
+           else
+             frame.sum
+           end
 end
-rub
+
 puts point
