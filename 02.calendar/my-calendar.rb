2bf8cc1abbbd9a884ca1c1aae5788230d4c75c48
harada-webdev <hrdk74531@outlook.jp>
2023-10-12 21:08:37 +0900

    corrected my-calendar.rb

--- 02.calendar/my-calendar.rb
+++ 02.calendar/my-calendar.rb
@@ -1,38 +1,38 @@
+# frozen_string_literal: true
+
 require 'date'
 require 'optparse'
 
 # -mで月を、-yで年を指定できるオプションの生成
-calenderOptions = {}
+calender_options = {}
 OptionParser.new do |options|
-  options.on("-y year",Integer) {|year| calenderOptions[:YEAR] = year}
-  options.on("-m month",Integer) {|month| calenderOptions[:MONTH] = month}
+  options.on('-y year', Integer) { |year| calender_options[:year] = year }
+  options.on('-m month', Integer) { |month| calender_options[:month] = month }
 end.parse!
 
 # 表示させるカレンダー(年月)の設定
-nowDate = Date.today
-selectedYear = calenderOptions[:YEAR] || nowDate.year
-selectedMonth = calenderOptions[:MONTH] || nowDate.month
+now_date = Date.today
+selected_year = calender_options[:year] || now_date.year
+selected_month = calender_options[:month] || now_date.month
 
 # どの年月にも対応した月初日と月末日のインスタンスの生成
-firstDay = Date.new(selectedYear, selectedMonth, 1)
-lastDay = Date.new(selectedYear, selectedMonth, -1)
+first_day = Date.new(selected_year, selected_month, 1)
+last_day = Date.new(selected_year, selected_month, -1)
 
 # 見出しの作成
-puts firstDay.strftime("%B %Y").center(20)
-puts "Su Mo Tu We Th Fr Sa"
+puts first_day.strftime('%B %Y').center(20)
+puts 'Su Mo Tu We Th Fr Sa'
 
 # 日付の配置(スペースを利用して、対応する曜日と日付を上下に揃えます)
 # 今日の日付の部分の色の反転
-print "\s\s\s" * firstDay.wday    
-(firstDay..lastDay).each do |oneday|
-  if oneday == nowDate && oneday.day < 10
-    print "\e[7m\s#{oneday.day}\e[0m\s" # 一桁の日付の数値が二桁目の場所に来ないように調整します
-  elsif 
-    oneday == nowDate && oneday.day >= 10
-    print "\e[7m#{oneday.day}\e[0m\s"
+print "\s\s\s" * first_day.wday
+(first_day..last_day).each do |oneday|
+  if oneday == now_date
+    print "\e[7m#{oneday.day.to_s.rjust(2)}\e[0m\s"
   else
-    print oneday.day < 10 ? "\s#{oneday.day}\s" : "#{oneday.day}\s"
+    print "#{oneday.day.to_s.rjust(2)}\s"
   end
-print "\n" if oneday.saturday? && oneday != lastDay end
+  print "\n" if oneday.saturday? && oneday != last_day
+end
 
 puts "\n\n"
