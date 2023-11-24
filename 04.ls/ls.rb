ac6c4710ae3f2205777f8c1574fbd62bd523f8a3
harada-webdev <hrdk74531@outlook.jp>
2023-11-24 15:55:40 +0900

    modified2_ls1

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 def fetch_file
-  Dir.glob('[^.]*')
+  Dir.glob('*')
      .sort
 end
 
 def order_vertically(rows = 4, cols = 3)
   entries = fetch_file
-  board = Array.new(rows) { Array.new(cols) }
+  file_table = Array.new(rows) { Array.new(cols) }
 
   entries.each_with_index do |entry, index|
     col, row = index.divmod(rows)
-    board[row][col] = entry
+    file_table[row][col] = entry
   end
 
-  board
+  file_table
 end
 
-file_list_board = order_vertically
-file_list_board.each do |file_row|
+formatted_file_table = order_vertically
+formatted_file_table.each do |file_row|
   puts file_row.map { |file_col| file_col.to_s.ljust(30) }.join
 end
