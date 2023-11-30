43fb38158365e250e36cf6241d5ef1dc4764da7f
harada-webdev <hrdk74531@outlook.jp>
2023-12-01 06:35:46 +0900

    add ls_4

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,17 +1,55 @@
 # frozen_string_literal: true
 
-show_reversed = ARGV.include?('-r')
+require 'etc'
 
-def fetch_file(show_reversed: false)
-  sorted_files = Dir.glob('*').sort
-  show_reversed ? sorted_files.reverse : sorted_files
+def exposit_file
+  sorted_files = Dir.glob('*').sort_by(&:downcase)
+  puts "total #{calculate_total_block_size(sorted_files)}"
+  display_file_mode(sorted_files)
 end
 
-def order_vertically(rows = 4, cols = 3, show_reversed: false)
-  entries = fetch_file(show_reversed:)
+def calculate_total_block_size(files)
+  total_block_size = 0
+
+  files.each do |file|
+    file_stat = File::Stat.new(file)
+    required_block_number = (file_stat.size / file_stat.blksize.to_f).ceil
+    total_block_size += ((required_block_number * file_stat.blksize) / 1024.0).ceil
+  end
+
+  total_block_size
+end
+
+def display_file_mode(files)
+  files.each do |file|
+    file_stat = File::Stat.new(file)
+    file_mode = change_mode_appearance(file_stat.mode)
+    file_mode = File.directory?(file) ? "d#{file_mode}" : "-#{file_mode}"
+    display_file_detail(file, file_stat, file_mode)
+  end
+end
+
+def change_mode_appearance(mode_number)
+  string_expression = ['---', '--x', '-w-', '-wx', 'r--', 'r-x', 'rw-', 'rwx']
+  mode_number.to_s(8)[-3..].chars.map { |number| string_expression[number.to_i] }.join
+end
+
+def display_file_detail(file, file_stat, file_mode)
+  puts ''\
+     "#{file_mode} "\
+     "#{file_stat.nlink} "\
+     "#{Etc.getpwuid(file_stat.uid).name} "\
+     "#{Etc.getgrgid(file_stat.gid).name} "\
+     "#{file_stat.size.to_s.rjust(4)} "\
+     "#{file_stat.mtime.strftime('%b %_d %H:%M')} "\
+     "#{file}"
+end
+
+def order_vertically(rows = 4, cols = 3)
+  sorted_files = Dir.glob('*').sort
   file_table = Array.new(rows) { Array.new(cols) }
 
-  entries.each_with_index do |entry, index|
+  sorted_files.each_with_index do |entry, index|
     col, row = index.divmod(rows)
     file_table[row][col] = entry
   end
@@ -19,7 +57,12 @@ def order_vertically(rows = 4, cols = 3, show_reversed: false)
   file_table
 end
 
-formatted_file_table = order_vertically(show_reversed:)
-formatted_file_table.each do |file_row|
-  puts file_row.map { |file_col| file_col.to_s.ljust(30) }.join
+show_detail = ARGV.include?('-l')
+if show_detail
+  exposit_file
+else
+  formatted_file_table = order_vertically
+  formatted_file_table.each do |file_row|
+    puts file_row.map { |file_col| file_col.to_s.ljust(30) }.join
+  end
 end
