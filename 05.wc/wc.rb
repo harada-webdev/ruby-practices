77682774c41f6e38d30a460bd7783820af3a08bc
harada-webdev <hrdk74531@outlook.jp>
2024-09-03 02:15:45 +0900

    simplified the code

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -3,91 +3,62 @@
 require 'optparse'
 
 def main
-  totals = { lines: 0, words: 0, chars: 0 }
   files, options = parse_command_line_arguments
+  totals = { lines: 0, words: 0, chars: 0 }
 
   files.each do |file_info|
     file = file_info[:file]
-    file_name = file_info[:file_name]
+    name = file_info[:name]
+    content = file.read
+    counts = { lines: content.split("\n").size, words: content.split.size, chars: content.size }
 
-    counts = generate_counts(file)
-    display_counts(counts, file_name, options)
-    update_totals(counts, options, totals)
+    display_counts(counts, name, options)
 
-    file.close unless file_name.nil?
+    %i[lines words chars].each { |key| totals[key] += counts[key] if options[key] }
+    file.close unless name.nil?
   end
 
   display_totals(options, totals) if files.count > 1
 end
 
 def parse_command_line_arguments
-  options = {}
+  options = { lines: false, words: false, chars: false }
 
   OptionParser.new do |opts|
-    opts.on('-l') { options[:l] = true }
-    opts.on('-w') { options[:w] = true }
-    opts.on('-c') { options[:c] = true }
+    opts.on('-l') { options[:lines] = true }
+    opts.on('-w') { options[:words] = true }
+    opts.on('-c') { options[:chars] = true }
 
     opts.parse!(ARGV)
   end
+  options.transform_values! { true } if options.values.all?(false)
 
   files = if ARGV.empty?
-            [{ file_name: nil, file: $stdin }]
+            [{ name: nil, file: $stdin }]
           else
             ARGV.map do |f|
-              { file_name: f, file: File.open(f) }
+              { name: f, file: File.open(f) }
             end
           end
 
   [files, options]
 end
 
-def generate_counts(file)
-  file_content = file.read
-  {
-    'l' => file_content.split("\n").size,
-    'w' => file_content.split.size,
-    'c' => file_content.size
-  }
-end
+def display_counts(counts, name, options)
+  line_counts = "#{counts[:lines].to_s.rjust(5)} " if options[:lines]
+  word_counts = "#{counts[:words].to_s.rjust(5)} " if options[:words]
+  char_counts = "#{counts[:chars].to_s.rjust(5)} " if options[:chars]
+  name = name.rjust(10).to_s unless name.nil?
 
-def display_counts(counts, file_name, options)
-  if options.empty?
-    puts "#{counts['l'].to_s.rjust(5)} " \
-         "#{counts['w'].to_s.rjust(5)} " \
-         "#{counts['c'].to_s.rjust(5)} " \
-         "#{file_name unless file_name.nil?}"
-  else
-    puts "#{counts['l'].to_s.rjust(5) if options[:l]} " \
-         "#{counts['w'].to_s.rjust(5) if options[:w]} " \
-         "#{counts['c'].to_s.rjust(5) if options[:c]} " \
-         "#{file_name unless file_name.nil?}"
-  end
-end
-
-def update_totals(counts, options, totals)
-  if options.empty?
-    totals[:lines] += counts['l']
-    totals[:words] += counts['w']
-    totals[:chars] += counts['c']
-  else
-    totals[:lines] += counts['l'] if options[:l]
-    totals[:words] += counts['w'] if options[:w]
-    totals[:chars] += counts['c'] if options[:c]
-  end
+  puts "#{line_counts}#{word_counts}#{char_counts}#{name}"
 end
 
 def display_totals(options, totals)
   extracted_totals = []
-
-  totals.each_with_index do |(_order, total), index|
-    break if !options.empty? && options.length <= index
-
-    extracted_totals << total
-  end
+  options.each { |key, boolean| extracted_totals << totals[key] if boolean }
 
   puts "#{extracted_totals.map(&:to_s).map { |total| total.rjust(5) }.join(' ')} " \
-       'totals'
+       "#{'total'.rjust(10)}"
 end
 
 main
