5ad3ef8e382f4b1663ea21c9ac36bd66200c79ec
harada-webdev <hrdk74531@outlook.jp>
2024-10-18 01:33:58 +0900

    Add calculate_score method to Game class

--- 06.bowling_object/lib/game.rb
+++ 06.bowling_object/lib/game.rb
@@ -20,4 +20,32 @@ class Game
     frames.last.concat(frame)
     frames
   end
+
+  def calculate_score(frames)
+    @frames = frames
+    score = 0
+    10.times do |index|
+      frame = Frame.new(@frames[index])
+      score += frame.score == 10 && index < 9 ? calculate_bonus_score(frame, index) : frame.score
+    end
+    score
+  end
+
+  private
+
+  def calculate_bonus_score(frame, index)
+    next_first_mark = Mark.new(@frames[index + 1][0]).score
+    next_second_mark = Mark.new(@frames[index + 1][1]).score
+    after_next_first_mark = Mark.new(@frames[index + 2][0]).score if index < 8
+
+    if frame.first_mark.score == 10
+      if next_first_mark == 10 && index < 8
+        20 + after_next_first_mark
+      else
+        10 + next_first_mark + next_second_mark
+      end
+    else
+      10 + next_first_mark
+    end
+  end
 end
