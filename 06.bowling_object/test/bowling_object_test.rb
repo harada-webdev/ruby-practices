048cb5ca9e350b062259c66b8bc799be9349472a
harada-webdev <hrdk74531@outlook.jp>
2024-10-17 23:54:41 +0900

    Define Frame class

--- 06.bowling_object/test/bowling_object_test.rb
+++ 06.bowling_object/test/bowling_object_test.rb
@@ -2,6 +2,7 @@
 
 require 'minitest/autorun'
 require_relative '../lib/mark'
+require_relative '../lib/frame'
 
 class BowlingObjectTest < Minitest::Test
   def test_mark_score1
@@ -12,5 +13,10 @@ class BowlingObjectTest < Minitest::Test
   def test_mark_score2
     mark = Mark.new('0')
     assert_equal 0, mark.score
+  end
+
+  def test_frame_score
+    frame = Frame.new(%w[0 10])
+    assert_equal 10, frame.score
   end
 end
