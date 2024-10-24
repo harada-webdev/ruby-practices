a67a290c4e4386419745455ade11a61f99903f6c
harada-webdev <hrdk74531@outlook.jp>
2024-10-25 04:28:21 +0900

    Put bonus_score in Frame

--- 06.bowling_object/lib/frame.rb
+++ 06.bowling_object/lib/frame.rb
@@ -14,4 +14,16 @@ class Frame
   def score
     [@first_mark.score, @second_mark.score, @third_mark.score].sum
   end
+
+  def bonus_score(frame, next_first_mark, next_second_mark, after_next_first_mark)
+    if frame.first_mark.score == 10
+      if next_first_mark == 10 && !after_next_first_mark.nil?
+        20 + after_next_first_mark
+      else
+        10 + next_first_mark + next_second_mark
+      end
+    else
+      10 + next_first_mark
+    end
+  end
 end
--- 06.bowling_object/lib/game.rb
+++ 06.bowling_object/lib/game.rb
@@ -31,24 +31,10 @@ class Game
         next_first_mark = @frames[index + 1].first_mark.score
         next_second_mark = @frames[index + 1].second_mark.score
         after_next_first_mark = @frames[index + 2].first_mark.score if index < 8
-        calculate_bonus_score(frame, next_first_mark, next_second_mark, after_next_first_mark)
+        frame.bonus_score(frame, next_first_mark, next_second_mark, after_next_first_mark)
       else
         frame.score
       end
-    end
-  end
-
-  private
-
-  def calculate_bonus_score(frame, next_first_mark, next_second_mark, after_next_first_mark)
-    if frame.first_mark.score == 10
-      if next_first_mark == 10 && !after_next_first_mark.nil?
-        20 + after_next_first_mark
-      else
-        10 + next_first_mark + next_second_mark
-      end
-    else
-      10 + next_first_mark
     end
   end
 end
