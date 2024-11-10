b05cf6135590515ae3e6ea776cd610eef87457a4
harada-webdev <hrdk74531@outlook.jp>
2024-11-10 20:16:56 +0900

    Delete modules

--- 07.ls_object/lib/long_format.rb
+++ 07.ls_object/lib/long_format.rb
@@ -1,20 +1,57 @@
 # frozen_string_literal: true
 
-require_relative 'file_information'
+require 'etc'
+require 'time'
 
 class LongFormat
-  include FileInformation
-
   def initialize(file)
     @file = file
   end
 
   def show_file
-    puts "#{type}#{permissions} " \
-         "#{nlink.to_s.rjust(2)} " \
-         "#{owner_name} #{group_name} " \
-         "#{size.to_s.rjust(4)} " \
-         "#{mtime} " \
-         "#{basename}"
+    puts "#{convert_file_type(file_stat)}#{add_permissions(file_stat)} " \
+         "#{file_stat.nlink.to_s.rjust(2)} " \
+         "#{Etc.getpwuid(file_stat.uid).name} #{Etc.getgrgid(file_stat.gid).name} " \
+         "#{file_stat.size.to_s.rjust(4)} " \
+         "#{File.mtime(@file).strftime('%b %e %H:%M').to_s.rjust(4)} " \
+         "#{File.basename(@file)}"
+  end
+
+  private
+
+  def file_stat
+    File::Stat.new(@file)
+  end
+
+  def convert_file_type(file_stat)
+    {
+      'fifo' => 'p',
+      'characterSpecial' => 'c',
+      'directory' => 'd',
+      'blockSpecial' => 'b',
+      'file' => '-',
+      'link' => 'l',
+      'socket' => 's'
+    }[file_stat.ftype]
+  end
+
+  def add_permissions(file_stat)
+    ocatal_mode = file_stat.mode.to_s(8)
+    permissions = ocatal_mode[-3..].chars.map do |mode|
+      ['---', '--x', '-w-', '-wx', 'r--', 'r-x', 'rw-', 'rwx'][mode.to_i]
+    end
+    add_special_permissions(ocatal_mode, permissions)
+    permissions.join
+  end
+
+  def add_special_permissions(ocatal_mode, permissions)
+    case ocatal_mode[2]
+    when '1'
+      permissions[2] = permissions[2].chop + (permissions[2][2] == 'x' ? 't' : 'T')
+    when '2'
+      permissions[1] = permissions[1].chop + (permissions[1][2] == 'x' ? 's' : 'S')
+    when '4'
+      permissions[0] = permissions[0].chop + (permissions[0][2] == 'x' ? 's' : 'S')
+    end
   end
 end
--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -3,31 +3,48 @@
 
 require 'optparse'
 require 'pathname'
-require_relative 'lib/option_parseable'
-require_relative 'lib/file_fetchable'
-require_relative 'lib/file_information'
 require_relative 'lib/default_format'
 require_relative 'lib/long_format'
 
 class Ls
-  include OptionParseable
-  include FileFetchable
-  include FileInformation
-
   def initialize
     @options = parse_options
-    @files = fetch_files(@options)
   end
 
   def show_files
+    files = fetch_files(@options)
     if @options[:long]
-      puts "total #{@files.sum { |file| block_size(file) }}"
-      @files.each { |file| LongFormat.new(file).show_file }
+      puts "total #{files.sum { |file| File::Stat.new(file).blocks / 2 }}"
+      files.each { |file| LongFormat.new(file).show_file }
     else
       files_2d_array = Array.new(4) { Array.new(3) }
-      @files.each_with_index { |file, index| files_2d_array[index % 4][index / 4] = file }
+      files.each_with_index { |file, index| files_2d_array[index % 4][index / 4] = file }
       files_2d_array.each { |files_array| DefaultFormat.new(files_array).show_file }
     end
+  end
+
+  private
+
+  def parse_options
+    options = { all: false, reverse: false, long: false }
+
+    OptionParser.new do |opts|
+      opts.on('-a') { options[:all] = true }
+      opts.on('-r') { options[:reverse] = true }
+      opts.on('-l') { options[:long] = true }
+    end.parse!
+
+    options
+  end
+
+  def fetch_files(options)
+    target_directory = Pathname(ARGV[0] || '.').join('*')
+    files = if options[:all]
+              Dir.glob(target_directory, File::FNM_DOTMATCH).sort_by { |file| file.sub(/^\./, '') }
+            else
+              Dir.glob(target_directory).sort
+            end
+    options[:reverse] ? files.reverse : files
   end
 end
 
