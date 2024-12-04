0780688776f18ded06edae75ad6ce673f8f5ac90
harada-webdev <141257112+harada-webdev@users.noreply.github.com>
2024-12-05 02:58:19 +0900

    Add LsFile

---
+++ 07.ls_object/lib/ls_file.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+require 'etc'
+require 'time'
+
+class LsFile
+  def initialize(file, target_directory, options)
+    @file = file
+    @target_directory = target_directory
+    @options = options
+  end
+
+  def block_size
+    File::Stat.new(@file).blocks / 2
+  end
+
+  def file_stat
+    File.lstat(@file)
+  end
+
+  def type
+    {
+      'fifo' => 'p',
+      'characterSpecial' => 'c',
+      'directory' => 'd',
+      'blockSpecial' => 'b',
+      'file' => '-',
+      'link' => 'l',
+      'socket' => 's'
+    }[file_stat.ftype]
+  end
+
+  def permissions
+    ocatal_mode = file_stat.mode.to_s(8)
+    permissions = ocatal_mode[-3..].chars.map do |mode|
+      ['---', '--x', '-w-', '-wx', 'r--', 'r-x', 'rw-', 'rwx'][mode.to_i]
+    end
+    add_special_permissions(ocatal_mode, permissions)
+    permissions.join
+  end
+
+  def hard_links
+    file_stat.nlink
+  end
+
+  def owner_name
+    Etc.getpwuid(file_stat.uid).name
+  end
+
+  def owner_group_name
+    Etc.getpwuid(file_stat.gid).name
+  end
+
+  def size_or_device_info
+    if file_stat.rdev.zero?
+      file_stat.size
+    else
+      "#{file_stat.rdev_major}, #{file_stat.rdev_minor}"
+    end
+  end
+
+  def last_modified_time
+    if Time.now.year == file_stat.mtime.year
+      file_stat.mtime.strftime('%b %e %H:%M')
+    else
+      file_stat.mtime.strftime('%b %e  %Y')
+    end
+  end
+
+  def name
+    if @file == @target_directory
+      '.'
+    elsif @file == @target_directory.parent
+      '..'
+    elsif File.symlink?(@file) && @options[:long]
+      "#{@file} -> #{File.readlink(@file)}"
+    else
+      File.basename(@file)
+    end
+  end
+
+  private
+
+  def add_special_permissions(ocatal_mode, permissions)
+    case ocatal_mode[2]
+    when '1'
+      permissions[2] = permissions[2].chop + (permissions[2][2] == 'x' ? 't' : 'T')
+    when '2'
+      permissions[1] = permissions[1].chop + (permissions[1][2] == 'x' ? 's' : 'S')
+    when '4'
+      permissions[0] = permissions[0].chop + (permissions[0][2] == 'x' ? 's' : 'S')
+    end
+  end
+end
--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -4,8 +4,7 @@
 require 'optparse'
 require 'pathname'
 require_relative 'lib/max_length'
-require_relative 'lib/default_format'
-require_relative 'lib/long_format'
+require_relative 'lib/ls_file'
 
 class Ls
   def initialize
@@ -17,12 +16,9 @@ class Ls
     files = fetch_files(@options)
     max_length = @options[:long] ? MaxLength.new(files).file_information : MaxLength.new(files).file_name
     if @options[:long]
-      puts "total #{files.sum { |file| File::Stat.new(file).blocks / 2 }}"
-      files.each { |file| LongFormat.new(file, @target_directory, max_length).show_file }
+      show_files_by_long_format(files, max_length)
     else
-      files_2d_array = Array.new(4) { Array.new(3) }
-      files[0..11].each_with_index { |file, index| files_2d_array[index % 4][index / 4] = file }
-      files_2d_array.each { |files_array| DefaultFormat.new(files_array, @target_directory, max_length).show_file }
+      show_files_by_default_format(files, max_length)
     end
   end
 
@@ -59,6 +55,33 @@ class Ls
       else
         [2, file.basename.sub(/^\./, '')]
       end
+    end
+  end
+
+  def show_files_by_long_format(files, max_length)
+    puts "total #{files.sum { |file| LsFile.new(file, @target_directory, @options).block_size }}"
+    files.each do |file|
+      ls_file = LsFile.new(file, @target_directory, @options)
+      puts "#{ls_file.type}" \
+           "#{ls_file.permissions} " \
+           "#{ls_file.hard_links.to_s.rjust(max_length[:nlink])} " \
+           "#{ls_file.owner_name.to_s.rjust(max_length[:username])} " \
+           "#{ls_file.owner_group_name.to_s.rjust(max_length[:groupname])} " \
+           "#{ls_file.size_or_device_info.to_s.rjust(max_length[:size])} " \
+           "#{ls_file.last_modified_time.to_s.rjust(5)} " \
+           "#{ls_file.name}"
+    end
+  end
+
+  def show_files_by_default_format(files, max_length)
+    files_2d_array = Array.new(4) { Array.new(3) }
+    files[0..11].each_with_index { |file, index| files_2d_array[index % 4][index / 4] = file }
+    files_2d_array.each do |files_array|
+      row_files = files_array.compact.map do |file|
+        LsFile.new(file, @target_directory, @options).name.ljust(max_length + 1)
+      end
+
+      puts row_files.join
     end
   end
 end
