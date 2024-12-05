6bcf87a52f043abdb62568c1ffc6ee394c8ea467
harada-webdev <141257112+harada-webdev@users.noreply.github.com>
2024-12-05 16:51:35 +0900

    Delete MaxLength

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -3,7 +3,6 @@
 
 require 'optparse'
 require 'pathname'
-require_relative 'lib/max_length'
 require_relative 'lib/ls_file'
 
 class Ls
@@ -14,11 +13,10 @@ class Ls
 
   def show_files
     files = fetch_files(@options)
-    max_length = @options[:long] ? MaxLength.new(files).file_information : MaxLength.new(files).file_name
     if @options[:long]
-      show_files_by_long_format(files, max_length)
+      show_files_by_long_format(files)
     else
-      show_files_by_default_format(files, max_length)
+      show_files_by_default_format(files)
     end
   end
 
@@ -58,22 +56,34 @@ class Ls
     end
   end
 
-  def show_files_by_long_format(files, max_length)
+  def show_files_by_long_format(files)
+    max_length = fetch_file_max_length(files)
     puts "total #{files.sum { |file| LsFile.new(file, @target_directory, @options).block_size }}"
     files.each do |file|
       ls_file = LsFile.new(file, @target_directory, @options)
       puts "#{ls_file.type}" \
            "#{ls_file.permissions} " \
-           "#{ls_file.hard_links.to_s.rjust(max_length[:nlink])} " \
-           "#{ls_file.owner_name.to_s.rjust(max_length[:username])} " \
-           "#{ls_file.owner_group_name.to_s.rjust(max_length[:groupname])} " \
+           "#{ls_file.hard_links.to_s.rjust(max_length[:hard_links])} " \
+           "#{ls_file.owner_name.to_s.rjust(max_length[:owner_name])} " \
+           "#{ls_file.owner_group_name.to_s.rjust(max_length[:owner_group_name])} " \
            "#{ls_file.size_or_device_info.to_s.rjust(max_length[:size])} " \
            "#{ls_file.last_modified_time.to_s.rjust(5)} " \
            "#{ls_file.name}"
     end
   end
 
-  def show_files_by_default_format(files, max_length)
+  def fetch_file_max_length(files)
+    ls_files = files.map { |file| LsFile.new(file, @target_directory, @options) }
+    {
+      hard_links: ls_files.map { |ls_file| ls_file.hard_links.to_s.length }.max,
+      owner_name: ls_files.map { |ls_file| ls_file.owner_name.to_s.length }.max,
+      owner_group_name: ls_files.map { |ls_file| ls_file.owner_group_name.to_s.length }.max,
+      size: ls_files.map { |ls_file| ls_file.size_or_device_info.to_s.length }.max
+    }
+  end
+
+  def show_files_by_default_format(files)
+    max_length = fetch_file_name_max_length(files)
     files_2d_array = Array.new(4) { Array.new(3) }
     files[0..11].each_with_index { |file, index| files_2d_array[index % 4][index / 4] = file }
     files_2d_array.each do |files_array|
@@ -83,6 +93,12 @@ class Ls
 
       puts row_files.join
     end
+  end
+
+  def fetch_file_name_max_length(files)
+    files[0, 11].map do |file|
+      LsFile.new(file, @target_directory, @options).name.to_s.length
+    end.max + 1
   end
 end
 
